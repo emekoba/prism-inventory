ee7de5568ca32f150ca628f02dc64d4e76a4a20e
emekoba <klojett.re@gmail.com>
2021-09-06 05:14:48 +0100

    fixed inventory and item counter in home

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,26 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Control } from "../../state/reducer";
 import "./homepage.css";
 
 export default function HomePage() {
+	const [inv_no, setInv_no] = useState(0);
+	const [it_no, setIt_no] = useState(0);
+
 	const control = useContext(Control);
+
+	useEffect(() => {
+		setInv_no(
+			Object.keys(control.state.inventories).filter(
+				(e) =>
+					control.state.inventories[e]["creator"] ===
+					control.state.currentUser.id
+			).length
+		);
+
+		setIt_no(
+			Object.keys(control.state.items).filter(
+				(e) =>
+					control.state.items[e]["creator"] === control.state.currentUser.id
+			).length
+		);
+	}, []);
 
 	return (
 		<div className="homepage">
 			<div className="home-stats">
-				{Object.keys(control.state.items).length}
-				{Object.keys(control.state.items).length !== 1 ? " Items" : " Item"}
+				{it_no}
+
+				{it_no !== 1 ? " Items" : " Item"}
 			</div>
 
 			<div className="home-stats">
-				{
-					Object.keys(control.state.inventories).filter(
-						(e) => e["creator"] !== control.state.currentUser.id
-					).length
-				}
-				{Object.keys(control.state.inventories).length !== 1
-					? " Inventories"
-					: " Inventory"}
+				{inv_no}
+
+				{inv_no !== 1 ? " Inventories" : " Inventory"}
 			</div>
 		</div>
 	);
